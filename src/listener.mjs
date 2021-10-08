b8caa3e9b25d9da51eb22ba89933f17c25263b73
Sergey <s.zababurin.v@gmail.com>
2021-10-08 18:40:34 +0300

    add template

--- src/listener.mjs
+++ src/listener.mjs
@@ -5,15 +5,16 @@ export default (docs = { }) => {
     return new Promise(async (resolve, reject) => {
         let manager = await import("https://zababurinsv.github.io/z-events/index.min.mjs");
 
-        template(bucket, docs);
+        template("bucket", docs);
         
         docs.worker.onmessage = msg => {
-
             console.log("tick", msg.data.tick);
         }
 
         docs.worker.onerror = function(event) {
+
             console.log('There is an error with your worker!', event);
+        
         }
 
         manager.events.await('/get_data', (event) => {
--- src/template.mjs
+++ src/template.mjs
@@ -1,6 +1,13 @@
-export default (type='undefined', docs = document.body) => {
+export default async ( type=" ", docs = { } ) => {
 	switch(type) {
 		case'bucket':
+			for(let i = 0; i < 4; i++) {
+				let item = await docs.get.template.bucket({
+					colums: 4,
+					position: i 
+				})
+				docs.bucket.container.appendChild(item)
+			}
 			break;
 		default:
 			console.warn('неизвестный темплейт')
